bee387320f04695692621145b424f83c75e16e09
i.tsarev <itsarev.111@gmail.com>
2026-03-30 13:34:07 +0300

    редактирование переменых

--- qa_guru/tests/test_login_qa_guru.py
+++ qa_guru/tests/test_login_qa_guru.py
@@ -2,6 +2,7 @@ from selene import (browser, be, have)
 import time
 from allure import epic, feature, story, title, description, dynamic
 from pytest import mark
+from config import config
 
 
 
@@ -10,11 +11,11 @@ class TestLogin:
     @description("Тест проверяет успешную авторизацию")
     def test_login_qa_guru(self):
         time.sleep(1)
-        browser.open('config.BASE_URL')
+        browser.open(config.BASE_URL)
         time.sleep(1)
-        browser.element('[name="email"]').should(be.blank).type("USER_LOGIN")
+        browser.element('[name="email"]').should(be.blank).type(config.USER_LOGIN)
         time.sleep(1)
-        browser.element('[name="password"]').should(be.blank).type('USER_PASSWORD').press_enter()
+        browser.element('[name="password"]').should(be.blank).type(config.USER_PASSWORD).press_enter()
         time.sleep(1)
         browser.element('[class="stream-title"]').should(have.text('QA.GURU | Курс-интенсив: ChatGPT для тестировщиков'))
         #('[class="stream-title"]') можем записать проще: ('.stream-title')
@@ -24,9 +25,9 @@ class TestLogin:
     @story("Авторизация на QA Guru")
     @description("Тест проверяет вывод Неверный пароль при неверных логопасах")
     def test_invalid_password(self):
-        browser.open('https://school.qa.guru/')
+        browser.open(config.BASE_URL)
         time.sleep(1)
-        browser.element('[name="email"]').should(be.blank).type("yurapwnz1992@yandex.ru")
+        browser.element('[name="email"]').should(be.blank).type(config.USER_LOGIN)
         time.sleep(1)
         browser.element('[name="password"]').should(be.blank).type('23fff34f').press_enter()
         browser.element('.btn-error').should(have.text('Неверный пароль'))
@@ -35,9 +36,9 @@ class TestLogin:
     @story("Авторизация на QA Guru")
     @description("Тест проверяет вывод Не заполнено поле Пароль при не заполненном пароле")
     def test_empty_password(self):
-        browser.open('https://school.qa.guru/')
+        browser.open(config.BASE_URL)
         time.sleep(1)
-        browser.element('[name="email"]').should(be.blank).type("yurapwnz1992@yandex.ru")
+        browser.element('[name="email"]').should(be.blank).type(config.USER_LOGIN)
         time.sleep(1)
         browser.element('[name="password"]').press_enter()
         browser.element('.btn-error').should(have.text('Не заполнено поле Пароль'))
