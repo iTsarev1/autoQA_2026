a7d9e3c3e5a7467c3b2be2f2428edab5cf1417aa
i.tsarev <itsarev.111@gmail.com>
2026-03-31 12:39:47 +0300

    homework

--- testing.py
+++ testing.py
@@ -1,4 +1,26 @@
+try:
+    x = float(input("Введите любое число: "))
+    if x > 0:
+        print("Число положительное")
+    elif x < 0:
+        print("Число отрицательное")
+    else:
+        print("Число равно нулю")
+except ValueError:
+    print("Вы ввели не верное значение")
+
+x = y = 1
+while x < 10:
+    while y < 10:
+        print(f'{x} * {y} = {x * y}', end = ' | ')
+        y += 1
+    print()
+    y = 1
+    x += 1
+
+print('-' * 100)
+
 for x in range(1, 10):
     for y in range(1, 10):
-        print(f'{x} * {y} = {x * y}', end = " | " )
+        print(f'{x} * {y} = {x * y}', end = ' | ')
     print()
