8536f52cf6e003f918c635ae3b98c49f2154e5b2
i.tsarev <itsarev.111@gmail.com>
2026-04-06 19:18:53 +0300

    homework 06.04.26

--- testing.py
+++ testing.py
@@ -1,13 +1,4 @@
-try:
-    x = float(input("Введите любое число: "))
-    if x > 0:
-        print("Число положительное")
-    elif x < 0:
-        print("Число отрицательное")
-    else:
-        print("Число равно нулю")
-except ValueError:
-    print("Вы ввели не верное значение")
+
 
 x = y = 1
 while x < 10:
@@ -18,9 +9,39 @@ while x < 10:
     y = 1
     x += 1
 
+
 print('-' * 100)
+
 
 for x in range(1, 10):
     for y in range(1, 10):
         print(f'{x} * {y} = {x * y}', end = ' | ')
     print()
+
+
+print('-' * 100)
+
+
+number = int(input('Введите любое целое число, а я выведу сумму цифр: '))
+s = 0
+while number > 0:
+    s += number % 10
+    number //= 10
+print('Сумма равна:', s)
+
+
+print('-' * 100)
+
+
+s = 'hello'
+for i in enumerate(s, 4):
+    print(i)
+
+print('-' * 100)
+
+
+s = 'hello'
+for i in s:
+    if i == 'l':
+        continue
+    print(i)
