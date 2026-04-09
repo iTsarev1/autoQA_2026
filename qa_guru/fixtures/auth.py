e7b9d05b5e3b7ff603c4f1fdcb569becbecaeed2
Царев Илья Владимирович <itsarev.111@gmail.com>
2026-04-09 16:43:02 +0300

    тема фикстуры, тест логин

--- qa_guru/fixtures/auth.py
+++ qa_guru/fixtures/auth.py
@@ -1,13 +1,23 @@
 import pytest
 
-
-def test_login():
-    pass
-
-
+@pytest.fixture
 def browser():
-    pass
+    print("Браузер!")
 
 
+@pytest.fixture
+def login_page(browser):
+    print("Логин пейдж!")
+
+
+@pytest.fixture
 def user():
-    return username, password
+    print("Юзер!")
+    return "username", "password"
+
+
+#берем фикстуру user и добавляем ее имя в аргументы нашего теста:
+def test_login(login_page, user):
+    username, password = user
+    assert username == "username"
+    assert password == "password"
