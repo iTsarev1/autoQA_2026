df4015acac6c0feb7a5152600e8c3ae345d45ff1
Царев Илья Владимирович <itsarev.111@gmail.com>
2026-04-12 20:07:47 +0300

    Занятие по типам данных. Библиотека random

---
+++ training/lesson_types.py
@@ -0,0 +1,15 @@
+#Математика
+import math
+
+print(math.pi)
+print("-" * 100)
+
+
+#Рандом
+#Часто используется при написании автотестов
+
+import random
+
+print(random.randint(0,100))
+print(random.randint(0,100))
+print(random.randint(0,100))
