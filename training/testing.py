efc59e89cae71817edee473a2cb36e1f4c3d908c
Царев Илья Владимирович <itsarev.111@gmail.com>
2026-04-30 16:23:08 +0300

    Циклы next

--- training/testing.py
+++ training/testing.py
@@ -1,8 +1,16 @@
+for i in range(10):
+    print(i)
+print('-' * 100)
+
+
+
+
+
 for x in range(1,10):
     for y in range(1,10):
         print(f'{x} * {y} = {x * y}', end = ' | ')
     print()
-
+print('-' * 100)
 
 
 x = int(input("Введите любое целое число: "))
@@ -11,3 +19,4 @@ while x > 0:
     s += x%10
     x = x//10
 print("Сумма цифр введенного числа равна", s)
+print('-' * 100)
