6fa7f74201846ac1aeed86ed4ab40ace5270360b
i.tsarev <itsarev.111@gmail.com>
2026-05-27 16:04:37 +0300

    lesson 'базовые возможности Selene и CSS / Xpath-селекторы '

--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -1,5 +1,13 @@
-from selene import browser
+from selene import browser, have
 
 
 def test_complete_todo():
-    browser.open('https://google.com')
+    browser.open('https://todomvc.com/examples/emberjs/todomvc/dist/')
+    browser.element('.new-todo').type('a').press_enter()
+    browser.element('.new-todo').type('b').press_enter()
+    browser.element('.new-todo').type('c').press_enter()
+    browser.all('.todo-list').should(have.size(1))
+    # should используется для проверок чего-то. Например, be.blank - поле ввода пустое (blank) перед тем, как вводить текст.
+    # should.visible - проверяет, что элемент видимый на странице
+    # should.not.visible - проверяет, что элемент НЕ видимый на странице
+    # should(have.size(1)) - Проверяет, что количество элементов в коллекции равно 1
