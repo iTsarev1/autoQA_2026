163b7f97c86489e8f5f0a0e9cbc573ced87a0deb
i.tsarev <itsarev.111@gmail.com>
2026-05-27 11:23:33 +0300

    вместо открытия/закрытия браузера добавил фикстуру 'setup_browser'

--- tests/test_login_qa_guru.py
+++ tests/test_login_qa_guru.py
@@ -1,42 +1,34 @@
-from selene import (browser, be, have)
+from selene import browser, be, have
 import time
 from allure import epic, feature, story, title, description, dynamic
 from pytest import mark
 from config import config
+from fixtures.auth import setup_browser
 
 
 
 class TestLogin:
     @story("Авторизация на QA Guru")
     @description("Тест проверяет успешную авторизацию")
-    def test_login_qa_guru(self):
-        time.sleep(1)
-        browser.open(config.BASE_URL)
-        time.sleep(1)
+    def test_login_qa_guru(self, setup_browser):
+        # browser.open(config.BASE_URL)
         browser.element('[name="email"]').should(be.blank).type(config.USER_LOGIN)
-        time.sleep(1)
         browser.element('[name="password"]').should(be.blank).type(config.USER_PASSWORD).press_enter()
-        time.sleep(1)
         browser.element('[class="stream-title"]').should(have.text('QA.GURU | Курс-интенсив: ChatGPT для тестировщиков'))
         #('[class="stream-title"]') можем записать проще: ('.stream-title')
-        time.sleep(3)
-        browser.close()
+
 
     @story("Авторизация на QA Guru")
     @description("Тест проверяет вывод Неверный пароль при неверных логопасах")
-    def test_invalid_password(self):
-        browser.open(config.BASE_URL)
-        time.sleep(1)
+    def test_invalid_password(self, setup_browser):
         browser.element('[name="email"]').should(be.blank).type(config.USER_LOGIN)
-        time.sleep(1)
         browser.element('[name="password"]').should(be.blank).type('23fff34f').press_enter()
         browser.element('.btn-error').should(have.text('Неверный пароль'))
-        browser.close()
+
 
     @story("Авторизация на QA Guru")
     @description("Тест проверяет вывод Не заполнено поле Пароль при не заполненном пароле")
-    def test_empty_password(self):
-        browser.open(config.BASE_URL)
+    def test_empty_password(self, setup_browser):
         time.sleep(1)
         browser.element('[name="email"]').should(be.blank).type(config.USER_LOGIN)
         time.sleep(1)
