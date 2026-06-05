cda863f48c9a837a56222f7232423a5c074df2ca
i.tsarev <itsarev.111@gmail.com>
2026-06-05 08:22:54 +0300

    repeat цикл while

--- training/testing.py
+++ training/testing.py
@@ -10,14 +10,35 @@ print('-'*50)
 # print('-'*50)
 
 
-'''Выведи какое число + - или =0'''
-try:
-    x = int(input("Введите любое число: "))
-    if x > 0:
-        print("Число положительное")
-    elif x < 0:
-        print("Число отрицательное")
-    else:
-        print("Число равно нулю")
-except ValueError:
-    print("чупакабра")
+# '''Выведи какое число + - или =0'''
+# try:
+#     x = float(input("Введите любое число: "))
+#     if x > 0:
+#         print("Число положительное")
+#     elif x < 0:
+#         print("Число отрицательное")
+#     else:
+#         print("Число равно нулю")
+# except ValueError:
+#     print("чупакабра")
+
+
+x = y = 1
+while x < 10:
+    while y < 10:
+        print(f'{x} x {y} = {x*y}', end=' | ')
+        y +=1
+    print()
+    y =1
+    x+=1
+print('-'*50)
+
+
+'''Посчитать сумму цифр введенного числа'''
+number = int(input("Введите целое число: "))
+s = 0
+while number > 0:
+    s += number %10
+    number//=10
+print(s)
+print('-'*50)
