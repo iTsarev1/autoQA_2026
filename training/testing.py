8126d0cc9978ef9513a1f0cdfeb9ec8d63eb5234
i.tsarev <itsarev.111@gmail.com>
2026-06-22 16:26:00 +0300

    repeat mutability

---
+++ training/testing.py
@@ -0,0 +1,8 @@
+словарь dict {ключ: значение}
+множества set ([], {}, "")
+списки list  []
+неизменяемые мн frozenset ()
+КОРТЕЖ tuple ()
+
+изменяемые: list, set, dict
+неизменяемые: str, int, frozenset, tuple
