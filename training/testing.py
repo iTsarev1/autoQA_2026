db058500fe20558a5add08e6468da5d4753438e2
i.tsarev <itsarev.111@gmail.com>
2026-06-22 17:43:15 +0300

    clean file testing.py

--- training/testing.py
+++
@@ -1,8 +0,0 @@
-словарь dict {ключ: значение}
-множества set ([], {}, "")
-списки list  []
-неизменяемые мн frozenset ()
-КОРТЕЖ tuple ()
-
-изменяемые: list, set, dict
-неизменяемые: str, int, frozenset, tuple
