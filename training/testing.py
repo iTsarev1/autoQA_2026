b20ebc18ccbfb30ac66205e30eb117332249d565
i.tsarev <itsarev.111@gmail.com>
2026-07-06 15:59:29 +0300

    homework lesson_6. test_dark_theme_by_time_and_user_choice()

--- training/testing.py
+++ training/testing.py
@@ -13,3 +13,18 @@ def test_dark_theme_by_time():
         is_dark_theme = False
 
     assert is_dark_theme is True
+
+def test_dark_theme_by_time_and_user_choice():
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
+    # 1. Проверяем наличие выбора пользователя (приоритет №1)
+    if dark_theme_enabled_by_user is not None:
+        is_dark_theme = bool(dark_theme_enabled_by_user)
+    else:
+        # 2. Если выбора нет, применяем логику времени (ночь с 22 до 6)
+        if current_time >= time(22) or current_time <= time(6):
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
+
+    assert is_dark_theme is True
