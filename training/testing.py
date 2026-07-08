e257cc77df6c6029108304fbe76eae9fc6e0d390
i.tsarev <itsarev.111@gmail.com>
2026-07-08 16:44:33 +0300

    homework lesson_6. test_find_suitable_user() 1 часть задания выполнена и разобрана

--- training/testing.py
+++ training/testing.py
@@ -52,15 +52,15 @@ def test_find_suitable_user():
     suitable_users = None
     for user in users:
         if user["name"] == "Olga":
-            print(user)
+            suitable_users = user
 
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
+    suitable_users = []
     for user in users:
         if user["age"] < 20:
-            print(user)
+            suitable_users.append(user)
 
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
