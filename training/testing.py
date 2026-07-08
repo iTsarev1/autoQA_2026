1833c686d24b0c44a8e001b08a42c820e57c6a3a
i.tsarev <itsarev.111@gmail.com>
2026-07-08 17:57:11 +0300

    homework lesson_6. очистил testing.py от предыдущих тестов

--- training/testing.py
+++ training/testing.py
@@ -1,41 +1,6 @@
 from datetime import time
 
 
-def test_dark_theme_by_time():
-    """
-    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
-    """
-    current_time = time(hour=23)
-    # Логика: ночь считается с 22:00 до 06:00.
-    if current_time >= time(22) or current_time <= time(6):
-        is_dark_theme = True
-    else:
-        is_dark_theme = False
-
-    assert is_dark_theme is True
-
-def test_dark_theme_by_time_and_user_choice():
-    current_time = time(hour=16)
-    dark_theme_enabled_by_user = True
-    # 1. Проверяем наличие выбора пользователя (приоритет №1)
-    if dark_theme_enabled_by_user is not None:
-        is_dark_theme = bool(dark_theme_enabled_by_user)
-    else:
-        # 2. Если выбора нет, применяем логику времени (ночь с 22 до 6)
-        if current_time >= time(22) or current_time <= time(6):
-            is_dark_theme = True
-        else:
-            is_dark_theme = False
-
-    assert is_dark_theme is True
-
-
-def get_user(user):
-    return user["name"]
-
-get_user({"name": "Olga", "age": 45})
-
-
 def test_find_suitable_user():
     """
     Найдите нужного пользователя по условиям в списке пользователей
