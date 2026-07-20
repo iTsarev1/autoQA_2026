3fbabe0d75ff9125faf5eaa3010a78a0ec25df32
i.tsarev <itsarev.111@gmail.com>
2026-07-20 16:45:12 +0300

    lesson 7.py скачивание файлов в директорию полный с настройками

--- training/Lesson 7/download_file.py
+++ training/Lesson 7/download_file.py
@@ -1,8 +1,22 @@
+import time
+
 from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-def test_download():
-    browser.open("http://github.com/pytest-dev/pytest/blob/main/README.rst")
-    browser.element("[data-testid='download-raw-button']").click()
+
+options = webdriver.ChromeOptions()
+prefs = {
+    "download.default_directory": "C:/Users/i.tsarev/PythonProjects/PythonProjectTsarev/tmp",
+    "download.prompt_for_download": False
+}
+options.add_experimental_option("prefs", prefs)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+browser.config.driver = driver
+
+# def test_download():
+browser.open("http://github.com/pytest-dev/pytest/blob/main/README.rst")
+browser.element("[data-testid='download-raw-button']").click()
+time.sleep(5)
