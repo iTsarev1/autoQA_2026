6e50cda72cfc1ee41337af4f844cf794fdaddc3b
i.tsarev <itsarev.111@gmail.com>
2026-07-20 16:31:23 +0300

    lesson 7.py тест на скачивание файлов

--- training/Lesson 7/download_file.py
+++ training/Lesson 7/download_file.py
@@ -1,2 +1,8 @@
 from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
+def test_download():
+    browser.open("http://github.com/pytest-dev/pytest/blob/main/README.rst")
+    browser.element("[data-testid='download-raw-button']").click()
