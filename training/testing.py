b37922e506290e07984e0639e39fab60d420652d
i.tsarev <itsarev.111@gmail.com>
2026-07-27 07:40:15 +0300

    repeat циклы итерации

--- training/testing.py
+++ training/testing.py
@@ -6,3 +6,10 @@ random_number = random.randint(1,10) # noqa
 while required_number != random_number:
     random_number = random.randint(1,10)
     print(f"Пользователь ввел число: {random_number}")
+
+
+iteration_count = 10
+i = 1
+while i < iteration_count:
+    print(f"Текущая итерация: {i}")
+    i +=1
