74f4f92b93a7c820dcf78422fc37f815da957a15
i.tsarev <itsarev.111@gmail.com>
2026-07-27 07:46:48 +0300

    repeat циклы for

--- training/testing.py
+++ training/testing.py
@@ -1,4 +1,5 @@
 import random
+from pprint import pprint
 
 required_number = 7
 random_number = random.randint(1,10) # noqa
@@ -6,6 +7,7 @@ random_number = random.randint(1,10) # noqa
 while required_number != random_number:
     random_number = random.randint(1,10)
     print(f"Пользователь ввел число: {random_number}")
+print('-' * 100)
 
 
 iteration_count = 10
@@ -13,3 +15,17 @@ i = 1
 while i < iteration_count:
     print(f"Текущая итерация: {i}")
     i +=1
+print('-' * 100)
+
+
+users = [
+    {"name": "Ilya", "age": 32},
+    {"name": "Elena", "age": 22},
+    {"name": "Vasya", "age": 5},
+    {"name": "Petya", "age": 3},
+    {"name": "Klava", "age": 24}
+]
+
+for user in users:
+    pprint(f"Пользователю {user['name']} {user['age']} лет")
+
