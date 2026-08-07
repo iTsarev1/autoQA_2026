b677b74cda06eec5828e1e6f9070528bae069b88
i.tsarev <itsarev.111@gmail.com>
2026-08-07 16:29:34 +0300

    download_file.py до изменений

--- training/Lesson 7/download_file.py
+++ training/Lesson 7/download_file.py
@@ -2,6 +2,7 @@ from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 
 options = webdriver.ChromeOptions()
@@ -11,9 +12,9 @@ prefs = {
 }
 options.add_experimental_option("prefs", prefs)
 
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 browser.config.driver = driver
 
-# def test_download():
 browser.open("http://github.com/pytest-dev/pytest/blob/main/README.rst")
 browser.element("[data-testid='download-raw-button']").click()
