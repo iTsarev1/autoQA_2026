cb76f3e421ca7c41e60b44a039a2d7ae62911f3b
i.tsarev <itsarev.111@gmail.com>
2026-08-23 17:06:49 +0300

    create script_os обозначил переменную current_file

--- training/Lesson 7/script_os.py
+++ training/Lesson 7/script_os.py
@@ -3,4 +3,9 @@ import os
 
 # Как найти файл. Работа с путями. Куда файл сохранился
 
-print(os.path.abspath(__file__))
+
+# скрипт где мы сейчас находимся
+# print(os.path.abspath(__file__))
+
+
+current_file = os.path.abspath(__file__)
