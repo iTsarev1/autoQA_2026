66501dc973d069f27d5725ae989ca0969b64f87a
i.tsarev <itsarev.111@gmail.com>
2026-08-24 17:18:02 +0300

    script_os добавил скрипт tmp_dir

--- training/Lesson 7/script_os.py
+++ training/Lesson 7/script_os.py
@@ -11,6 +11,9 @@ import os
 current_file = os.path.abspath(__file__)
 
 current_dir = os.path.dirname(current_file)
-print(current_dir) # папка, где находится
+print(current_dir) # папка, где находится объект
 # куда бы мы не скопировали наш проект, сможем узнать куда нам внутри этого проекта распределить наши tmp,
 # наши файлы скачать/зачитать, сохраненки делать
+
+tmp_dir = os.path.join(current_dir, 'tmp')
+print(tmp_dir)
