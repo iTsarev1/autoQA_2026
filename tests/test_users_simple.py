832594d8588b34217b579af41c3047685ec01f8c
i.tsarev <itsarev.111@gmail.com>
2026-09-03 16:50:02 +0300

    def test_users_are_adults() написан, работает

--- tests/test_users_simple.py
+++ tests/test_users_simple.py
@@ -6,5 +6,6 @@ def test_users_are_adults():
     with open("users.csv") as f:
         users = csv.DictReader(f)
         # для каждого юзера из списка юзеров, если статус этого юзера = 'worker', возьми его и положи в результирующий список
-        worker = [user for user in users if user["status"] == "worker"]
-    print()
+        workers = [user for user in users if user["status"] == "worker"]
+    for worker in workers:
+        assert int(worker["age"]) >= 18
