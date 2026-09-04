771b072efc6dc0cf9fd7a5fd4c00bdd3d5d73367
i.tsarev <itsarev.111@gmail.com>
2026-09-04 08:47:50 +0300

    def test_users_are_adults() до фикстур

--- tests/test_users_simple.py
+++ tests/test_users_simple.py
@@ -3,12 +3,9 @@ import csv
 
 # Тест проверяет, что все работники совершеннолетние
 def test_users_are_adults():
-    try:
-        with open("users.csv") as f:
-            users = csv.DictReader(f)
-            # для каждого юзера из списка юзеров, если статус этого юзера = 'worker', возьми его и положи в результирующий список
-            workers = [user for user in users if user["status"] == "worker"]
-        for worker in workers:
-            assert int(worker["age"]) >= 18
-    except:
-        print(f"работник {worker["name"]} не совершеннолетний")
+    with open("users.csv") as f:
+        users = csv.DictReader(f)
+        # для каждого юзера из списка юзеров, если статус этого юзера = 'worker', возьми его и положи в результирующий список
+        workers = [user for user in users if user["status"] == "worker"]
+    for worker in workers:
+        assert int(worker["age"]) >= 18
