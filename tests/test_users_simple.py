097fec58421d2f5d3a5bd60b7a2757442b4192bf
i.tsarev <itsarev.111@gmail.com>
2026-09-04 08:40:00 +0300

    def test_users_are_adults() с блоком try, except

--- tests/test_users_simple.py
+++ tests/test_users_simple.py
@@ -1,11 +1,14 @@
 import csv
 
 
-# Тест проверяет, что все работники старше 18 лет
+# Тест проверяет, что все работники совершеннолетние
 def test_users_are_adults():
-    with open("users.csv") as f:
-        users = csv.DictReader(f)
-        # для каждого юзера из списка юзеров, если статус этого юзера = 'worker', возьми его и положи в результирующий список
-        workers = [user for user in users if user["status"] == "worker"]
-    for worker in workers:
-        assert int(worker["age"]) >= 18
+    try:
+        with open("users.csv") as f:
+            users = csv.DictReader(f)
+            # для каждого юзера из списка юзеров, если статус этого юзера = 'worker', возьми его и положи в результирующий список
+            workers = [user for user in users if user["status"] == "worker"]
+        for worker in workers:
+            assert int(worker["age"]) >= 18
+    except:
+        print(f"работник {worker["name"]} не совершеннолетний")
