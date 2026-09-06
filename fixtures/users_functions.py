e02a45e8643875c0ed057b27fbdb09df3ac13bb8
i.tsarev <itsarev.111@gmail.com>
2026-09-06 06:27:24 +0300

    def test_users_are_adults_2() добавил описание

--- fixtures/users_functions.py
+++ fixtures/users_functions.py
@@ -9,10 +9,16 @@ def users():
 
 @pytest.fixture
 def workers(users):
+    """
+    Берем только работников из списка пользователей
+    """
     workers = [user for user in users if user["status"] == "worker"]
     return workers
 
 
 def test_users_are_adults_2(workers):
+    """
+    Проверяем, что работники совершеннолетние
+    """
     for worker in workers:
         assert int(worker["age"]) >= 18
