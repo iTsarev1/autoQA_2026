187daadf7034d6efa7531f06e0c530c100556014
i.tsarev <itsarev.111@gmail.com>
2026-09-07 18:02:03 +0300

    test_users_are_adults_2

--- tests/test_users_function.py
+++ tests/test_users_function.py
@@ -1,9 +1,10 @@
 from fixtures.users_functions import *
 import pytest
+from allure import epic, feature, story, title, description, dynamic
 
+
+@story("Функциональность работников")
+@description("Тест проверяет, что работники совершеннолетние")
 def test_users_are_adults_2(workers):
-    """
-    Тест проверяет, что работники совершеннолетние
-    """
     for worker in workers:
         assert int(worker["age"]) >= 18
