c9435c3839aa454620f2e83e482938ebcdc84d35
i.tsarev <itsarev.111@gmail.com>
2026-09-10 07:56:48 +0300

    в users_functions.py отредактировал фикстуры -> list[User]:

--- fixtures/users_functions.py
+++ fixtures/users_functions.py
@@ -1,20 +1,27 @@
 import pytest
 import csv
+from models.users import User
 
 
 @pytest.fixture
-def users():
+def users() -> list[User]:
     with open("users.csv") as f:
         users = list(csv.DictReader(f))
-    return users
+    return [
+        User(name=user["name"],
+             age=int(user["age"]),
+             status=user["status"],
+             items=user["items"])
+        for user in users
+    ]
 
 
 @pytest.fixture
-def workers(users):
+def workers(users) -> list[User]:
     """
     Берем только работников из списка пользователей
     """
-    workers = [user for user in users if user["status"] == "worker"]
+    workers = [user for user in users if user.status == "worker"]
     return workers
 
 
